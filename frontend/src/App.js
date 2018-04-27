1fad41d51a76b65c581a177a5291eb4a5ce9f8b4
micahzev <friedland.micah@gmail.com>
2018-04-27 17:09:37 +0200

    ajhsj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,32 @@ import logo_alpha from './logo_alph.png';
 import './App.css';
 
 class App extends Component {
+
+  this.state = {
+    title:null
+  }
+
+  _handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.apiCall(e.target.value);
+    }
+  }
+
+  apiCall(URL){
+
+    fetch('http://localhost:5000/extract?url='+URL+'/', {mode: 'cors'})
+      .then(
+        function(response) {
+          response.json().then(function(data) {
+            console.log(data);
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,9 +46,9 @@ class App extends Component {
         <p className="App-intro">
           <div className="slide">
             <div className="slide-inner">
-              <form className="cooltext">
-                <input className=" cooltext cooltextinput" type="text" name="name" />
-            </form>
+              <div className="cooltext">
+                <input onKeyPress={this._handleKeyPress.bind(this)} className=" cooltext cooltextinput" type="text" name="name" />
+            </div>
             </div>
           </div>
         </p>
