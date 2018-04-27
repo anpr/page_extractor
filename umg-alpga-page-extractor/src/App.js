24ac5d46c83b374ab7d1dafc51f6accd5bed47ce
micahzev <friedland.micah@gmail.com>
2018-04-27 17:20:57 +0200

    show title

---
+++ umg-alpga-page-extractor/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import logo from './logo_umg.png';
+import logo_alpha from './logo_alph.png';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null
+    }
+  }
+
+  _handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.apiCall(e.target.value);
+    }
+  }
+
+  apiCall(URL){
+
+    fetch('http://localhost:5000/extract?url='+URL+'/', {mode: 'cors'})
+      .then(
+        function(response) {
+          response.json().then(function(data) {
+            console.log(data);
+            this.setState({
+              title: data.title
+            })
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <h1 className="App-title">Universal Music & Alpha</h1>
+            <h2 className="App-title">present</h2>
+            <h3 className="App-title">Press Source Data Extraction Tool</h3>
+            <h4 className="App-title">#musicathon</h4>
+          </div>
+          <img src={logo_alpha} className="App-logo" alt="logo" />
+        </header>
+        <p className="App-intro">
+          <div className="slide">
+            <div className="slide-inner">
+              <div className="cooltext">
+                <input onKeyPress={this._handleKeyPress.bind(this)} className=" cooltext cooltextinput" type="text" name="name" />
+            </div>
+            {this.state.title?
+              <h6>{this.state.title}</h6>
+               : null}
+            </div>
+          </div>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
