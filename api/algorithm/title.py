ef8dc35d9d4ba4b1f3b8b92e37330c9ef59513d7
Andreas Profous <andreas.profous@kreuzwerker.de>
2018-04-27 16:14:41 +0200

    Title adjustment

--- api/algorithm/title.py
+++ api/algorithm/title.py
@@ -22,19 +22,30 @@ substitutes = [
 translation = "".maketrans("»«",'""')
 
 
-def fetch_title(soup: BeautifulSoup) -> str:
+def fetch_title_string(string_list):
+    if len(string_list) == 0:
+        return None
+    out = ''
+    index = -1
+    while len(out.strip()) < 3:
+        out = string_list[index]
+        index -= 1
+    return out
+
+
+def fetch_title(soup):
     h1 = None
     # 1 rule
     try:
         for h in soup.find_all("h1",limit=3):
 
-            if 'site-title' not in h['class']:
+            if 'header' not in [c.name for c in h.parents] and 'site-title' not in h['class'] and "is-hidden" not in h['class']:
                 h1 = h
                 break
     except KeyError:
         h1 = h
 
-    title = h1 and (h1.string or list(h1.strings)[-2])
+    title = h1 and (h1.string or fetch_title_string(list(h1.strings)))
 
     depth = 0
     if h1:
@@ -47,7 +58,24 @@ def fetch_title(soup: BeautifulSoup) -> str:
                 break
 
     if not title or len(title) < 3:
-        title = soup.h2 and soup.h2.string
+        title = soup.h2 and (soup.h2.string or fetch_title_string(list(soup.h2.strings)))
+        for i in soup.h2.parents:
+            try:
+                if i['id'] == 'header':
+                    title = None
+                    break
+            except KeyError:
+                pass
+
+    if not title or len(title) < 3:
+        title = soup.h3 and (soup.h3.string or fetch_title_string(list(soup.h3.strings)))
+        for i in soup.h3.parents:
+            try:
+                if i['id'] == 'header':
+                    title = None
+                    break
+            except KeyError:
+                pass
 
 
     # 2nd rule
